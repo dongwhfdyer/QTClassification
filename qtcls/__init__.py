d9c498dcbca61a2ab134e286510e7ed2d465ac88
horrible-dong <954513308@qq.com>
2023-03-19 22:25:33 +0800

    20230319 qtcls v0.1.0

--- qtcls/__init__.py
+++ qtcls/__init__.py
@@ -1,1 +1,6 @@
+# ********************************************
+# ********  QTClassification Toolkit  ********
+# ********************************************
+# Copyright (c) QIU, Tian. All rights reserved.
+
 __version__ = "0.1.0"
